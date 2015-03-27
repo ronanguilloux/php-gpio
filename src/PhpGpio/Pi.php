82d669857337096930a858650ba3630a6aafa222
Tjoosten <Tjoosten3@gmail.com>
2015-03-28 00:34:13 +0100

    Add docblocks

--- src/PhpGpio/Pi.php
+++ src/PhpGpio/Pi.php
@@ -5,12 +5,12 @@ namespace PhpGpio;
 class Pi
 {
     /**
-     * 
+     *
      * Get RaspberryPi version
-     * 
-     * A list of Model and Pi Revision & Hardware Revision Code from '/proc/cpuinfo' is here: 
+     *
+     * A list of Model and Pi Revision & Hardware Revision Code from '/proc/cpuinfo' is here:
      * @link http://www.raspberrypi-spy.co.uk/2012/09/checking-your-raspberry-pi-board-version/
-     * 
+     *
      * @return decimal Raspi version
      */
     public function getVersion()
@@ -24,36 +24,56 @@ class Pi
 
         return 0;
     }
-	
+
+    /**
+     * Get the CPu load of the Raspberry.
+     *
+     * @return string.
+     */
     public function getCpuLoad()
     {
         return sys_getloadavg();
     }
-	
+
+    /**
+     * Get the Cpu temperature
+     *
+     * @return integer.
+     */
     public function getCpuTemp($fahrenheit = false)
     {
         $cputemp = floatval(file_get_contents('/sys/class/thermal/thermal_zone0/temp'))/1000;
-		
+
 		if($fahrenheit)
 			$cputemp = 1.8* $cputemp+32;
-		
+
         return $cputemp;
     }
-	
+
+    /**
+     * Get the temperature of the Graphical Porcessing Unit.
+     *
+     * @return integer.
+     */
     public function getGpuTemp($fahrenheit = false)
     {
         $gputemp = floatval(str_replace(array('temp=', '\'C'), '', exec('/opt/vc/bin/vcgencmd measure_temp')));
-		
+
 		if($fahrenheit)
 			$gputemp = 1.8* $gputemp+32;
 
         return $gputemp;
     }
-	
+
+    /**
+     *  Get the frequency of the CPU.
+     *
+     * @return integer
+     */
     public function getCpuFrequency()
     {
         $frequency = floatval(file_get_contents('/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq'))/1000;
-		
+
         return $frequency;
     }
 }
